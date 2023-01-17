605cb8c4d6b284aa0aecb74210d51dc9d925dd0f
Ermias Gashu <gearmias@gmail.com>
2023-01-17 15:58:50 +0300

    added the tests and functionalities for list and buy of items

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,68 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+    string public name;
+    address public owner;
+
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    mapping(uint256 => Item) public items;
+
+    event List(string name, uint256 cost, uint256 quantity);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor() {
+        name = "Dappazon";
+        owner = msg.sender;
+    }
+
+    // List products
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _catgory,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public onlyOwner {
+        // make sure the one in charge of adding items is only the owner
+
+        // Create new item object from params
+        Item memory _item = Item(
+            _id,
+            _name,
+            _catgory,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+
+        // Save the newly created item in our mapping
+        items[_id] = _item;
+
+        // emit the List event as the new item is added
+        emit List(_name, _cost, _stock);
+    }
+
+    // Buy products
+    function buy(uint256 _id) public payable {
+        
+    }
+
+    // Withdraw funds
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,64 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
+// Global values to be used as item info
+const ID = 1;
+const NAME = "Shoe";
+const CATEGORY = "Closing";
+const IMAGE = "IMAGE";
+const COST = tokens(1);
+const RATING = 4;
+const STOCK = 5;
 
 describe("Dappazon", () => {
+  let dappazon;
+  let deployer;
+  let buyer;
+  // This block runs before each test
+  beforeEach(async () => {
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    dappazon = await Dappazon.deploy();
+    [deployer, buyer] = await ethers.getSigners();
+    // console.log(deployer.address, buyer.address);
+  });
+  describe("Deployment for Dappazon", () => {
+    it("Check if the deployer and the owner have the same address", async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address);
+    });
+  });
 
-})
+  describe("Listing", () => {
+    let transactions;
+    beforeEach(async () => {
+      transactions = await dappazon
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+
+      await transactions.wait();
+
+      transactions = await dappazon.connect(buyer).buy(ID, { value: COST });
+      await transactions.wait();
+    });
+
+    it("Returns item attribute", async () => {
+      const itemOne = await dappazon.items(1);
+      expect(itemOne.id).to.equal(1);
+      expect(itemOne.category).to.equal(CATEGORY);
+      // we can test all the attributes if they are equal to the ones we have provided them
+    });
+
+    // check if the event is emitted on new item added into mapping
+    it("Emits the List event", async () => {
+      expect(transactions).to.emit(dappazon, "List");
+    });
+
+    it("The value of the dappazone contract ether amount shoud increase!", async () => {
+      const amount = await ethers.provider.getBalance(dappazon.address);
+      expect(amount).to.equal(COST);
+    });
+  });
+});
